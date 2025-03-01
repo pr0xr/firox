9a7a4ae5a3c516f7afb9ee0be0563cf0f1b08771
Paul Liljenberg <62352+pr0xr@users.noreply.github.com>
2025-03-01 09:45:07 +0100

    Update download.ts

--- deno/download.ts
+++ deno/download.ts
@@ -11,9 +11,10 @@ if (!HOME_DIR) {
 
 const BIN_DIR = join(HOME_DIR, ".bin");
 await ensureDir(BIN_DIR);
-
+https://caido.download/releases/v0.46.0/caido-cli-v0.46.0-linux-x86_64.tar.gz
 const FILES = [
   { url: "https://github.com/epi052/feroxbuster/releases/download/v2.11.0/x86_64-linux-feroxbuster.tar.gz", name: "x86_64-linux-feroxbuster.tar.gz", executable: "feroxbuster" },
+  { url: "https://caido.download/releases/v0.46.0/caido-cli-v0.46.0-linux-x86_64.tar.gz", name: "caido-cli-v0.46.0-linux-x86_64.tar.gz", executable: "caido" },
 ];
 
 async function downloadFile(url: string, outputPath: string) {

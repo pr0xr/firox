69ab3d0b7d7ef94eecefc4784d9e1c1e7faa51a1
pr0xr <paul@bangofbits.com>
2025-07-15 14:22:49 -0400

    bump

--- python/oneliners.sh
+++ python/oneliners.sh
@@ -1,1 +1,1 @@
-curl --proto '=https' --tlsv1.2 -LsSf https://github.com/astral-sh/uv/releases/download/0.6.3/uv-installer.sh | sh
+curl --proto '=https' --tlsv1.2 -LsSf https://github.com/astral-sh/uv/releases/download/0.7.21/uv-installer.sh | sh
